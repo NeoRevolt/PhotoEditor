d18323889fcf112842e85e370948876f7875706f
NeoRevolt <dzalfikriali@gmail.com>
2022-12-02 17:23:27 +0700

    Add RemoteImagesActivity 2.kt

---
+++ app/src/main/java/com/example/photoediting/ui/RemoteImagesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.photoediting.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import example.photoediting.R
+
+class RemoteImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remote_images)
+    }
+}
